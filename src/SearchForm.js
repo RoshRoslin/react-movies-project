7cd95ed295f1a24455e82d181394e95de51bf0e1
RoshRoslin <RoshRoslin@gmail.com>
2021-05-17 14:33:19 -0700

    first push, source code

---
+++ src/SearchForm.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useGlobalContext } from './context';
+const SearchForm = () => {
+  const { query, setQuery, error } = useGlobalContext();
+
+  return (
+    <form
+      action=''
+      className='search-form'
+      onSubmit={(event) => event.preventDefault()}
+    >
+      <h2>Search Movies</h2>
+      <input
+        type='text'
+        className='form-input'
+        value={query}
+        onChange={(event) => setQuery(event.target.value)}
+      />
+      {error.show && <div className='error'>{error.msg}</div>}
+    </form>
+  );
+};
+
+export default SearchForm;
