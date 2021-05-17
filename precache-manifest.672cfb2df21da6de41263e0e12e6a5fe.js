038be773c74bc13992ac63d2ae39376299673d26
RoshRoslin <RoshRoslin@gmail.com>
2021-05-17 14:32:16 -0700

    Updates

---
+++ precache-manifest.672cfb2df21da6de41263e0e12e6a5fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6609d237a0be3e10d59b4139bb6a0a",
+    "url": "/react-movies-project/index.html"
+  },
+  {
+    "revision": "d29c0510e9f239849cca",
+    "url": "/react-movies-project/static/css/main.289c1fa7.chunk.css"
+  },
+  {
+    "revision": "290aa4f4295c4e36967e",
+    "url": "/react-movies-project/static/js/2.33260d0b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-movies-project/static/js/2.33260d0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d29c0510e9f239849cca",
+    "url": "/react-movies-project/static/js/main.05451aea.chunk.js"
+  },
+  {
+    "revision": "d979759ec3e73fe044a4",
+    "url": "/react-movies-project/static/js/runtime-main.f2e8db6a.js"
+  }
+]);
